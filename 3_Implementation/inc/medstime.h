b821fceb5ff50eff9315ba7d310cded0b533d4f7
99002486 <71490592+99002486@users.noreply.github.com>
2020-11-12 12:57:31 +0530

    Update medstime.h

--- 3_Implementation/inc/medstime.h
+++ 3_Implementation/inc/medstime.h
@@ -1,6 +1,11 @@
 #ifndef __MEDSTIME_H
 #define __MEDSTIME_H
 
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<string.h> 
+#include<time.h>
+
 int readCSVData(char *buff, char *tim);
 
 int medCheck(void);
