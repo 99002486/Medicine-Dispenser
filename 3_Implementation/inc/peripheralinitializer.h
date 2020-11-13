c2ffe8fdd8299ce69291673c06d6a316a8711b59
99002486 <71490592+99002486@users.noreply.github.com>
2020-11-13 10:12:48 +0530

    Update peripheralinitializer.h

--- 3_Implementation/inc/peripheralinitializer.h
+++ 3_Implementation/inc/peripheralinitializer.h
@@ -14,11 +14,39 @@ typedef struct peripherals
     FILE* motorout2;
 } peripherals_t;
 
+/**
+@function-peripheralinit
+@param-void
+@return-peripherals_t, peripheral handling structure
+@description - Opens all the respective files for all the peripherals needed and returns a structure with all the file pointers
+**/
+
 peripherals_t peripheralsinit(void);
+
+/**
+@function-peripheral_setin
+@param-FILE* peripheral_ddr, file pointer to the direction file of GPIO pin 
+@return-int
+@description - Sets the direction of the GPIO as output
+**/
 
 int peripheral_setout(FILE* peripheral_ddr);
 
+/**
+@function-peripheral_setin
+@param-FILE* peripheral_ddr, file pointer to the direction file of GPIO pin 
+@return-int
+@description - Sets the direction of the GPIO as input
+**/
+
 int peripheral_setin(FILE* peripheral_ddr);
+
+/**
+@function-peripheralddrinit
+@param-void
+@return-int
+@description - Sets the direction for all the GPIO pins needed to be used.
+**/
 
 int peripheralddrinit(void);
 
