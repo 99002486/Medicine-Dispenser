de044f972f8ceb82ac87b8b664328e4b5f74dbc7
99002496 <71483090+99002496@users.noreply.github.com>
2020-11-13 10:04:22 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -9,51 +9,51 @@ int main()
     	struct peripherals_t periph;					//Initializes peripheral handler structure
 	while(1)
 	{
-		int med_number;
-		periph=peripheralinit();
-		med_number=medstime();
+		int med_number;						//Variable to fetch the medicine number
+		periph=peripheralinit();				//Initializes all the peripherals
+		med_number=medstime();					//Fetching the medicine number from csv file
 		delay(1);
-		printf("medicine number = %d\n",med_number);	
-		if(med_number>0 && MED_FLAG==0)
+		printf("medicine number = %d\n",med_number);		//Printing the received medicine number
+		if(med_number>0 && MED_FLAG==0)				//Checking whether the medicine number is received 				
 		{
-			MED_FLAG=1;
+			MED_FLAG=1;					//Setting the MED_FLAG once the medicine number is received
 			//SPI_transmit(med_number);
 		}
-        	fscanf(pirin,"%d",&pir_val);
+        	fscanf(pirin,"%d",&pir_val);				//Checking for PIR value
 		if(MED_FLAG)
 		{	
 			while(PIR_FLAG==0)
 			{
 
-				fprintf(periph.rled, "%d",1);
-				fflush(periph.rled);
+				fprintf(periph.rled, "%d",1);		
+				fflush(periph.rled);			//Red LED is on to indicate the time to take the medicine
 				delay(1);
-				if(periph.pir>0 && PIR_FLAG==0)
+				if(periph.pir>0 && PIR_FLAG==0)		//Checking whether the PIR input is received
 				{
-					PIR_FLAG=1;
-					fprintf(periph.rled,"%d",0);
-					fflush(periph.rled);
+					PIR_FLAG=1;			//Setting the PIR_FLAG when the PIR input is high
+					fprintf(periph.rled,"%d",0);	
+					fflush(periph.rled);		//Turning off the Red LED once the PIR detects motion
 
 				}
 			}
 
-		    fprintf(periph.gled, "%d",1);
-		    fflush(periph.gled);
+		    fprintf(periph.gled, "%d",1);			
+		    fflush(periph.gled);				//Turning on Green LED to indicate the dispensing of medicine
 		    delay(2);
 		    //fprintf(p1.gled, %d,0);
 		    //fflush(p1.gled);
 		    //delay(2);
-		    fprintf(periph.motorout1,"%d",1);
-		    fflush(periph.motorout1);
-		    fprintf(periph.motorout1,"%d",0);
-		    fflush(periph.motorout2);
+		    fprintf(periph.motorout1,"%d",1);			//Turning on Motor 		
+		    fflush(periph.motorout1);				//Giving high value to IN1 pin of motor driver
+		    fprintf(periph.motorout2,"%d",0);
+		    fflush(periph.motorout2);				//Giving low value to IN2 pin of motor driver
 		    delay(2);
-		    fprintf(periph.motorout1,"%d",0);
-		    fflush(periph.motorout1);
-		    fprintf(periph.motorout1,"%d",0);
-		    fflush(periph.motorout2);
-		    fprintf(periph.gled, "%d",0);
-		    fflush(periph.gled);
+		    fprintf(periph.motorout1,"%d",0);			//Turning off Motor
+		    fflush(periph.motorout1);				//Giving low value to IN1 pin of motor driver
+		    fprintf(periph.motorout2,"%d",0);			
+		    fflush(periph.motorout2);				//Giving low value to IN2 pin of motor driver
+		    fprintf(periph.gled, "%d",0);			
+		    fflush(periph.gled);				//Turning off green LED once the dispensing of medicine is complete
 		    delay(2);
         	}
         	PIR_FLAG=0;
