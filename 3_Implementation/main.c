4d678d8ce47b7438ac7a5a3c8e90a84e11b3c2fb
99002486 <71490592+99002486@users.noreply.github.com>
2020-11-13 09:47:08 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,17 +1,19 @@
 #include "peripheralinitializer.h"
 #include "medstime.h"
-
+/**Main function**/
 int main()
 {
-	int MED_FLAG=0;
-	int PIR_FLAG=0;
-	peripheralddrinit();
-    	struct peripherals_t periph;
+	int MED_FLAG=0;							//Flag for executing functionalities if medicine time is a match
+	int PIR_FLAG=0;							//Flag for executing functionalities if pir has value high
+	peripheralddrinit();						//Initializes direction of every peripheral
+    	struct peripherals_t periph;					//Initializes peripheral handler structure
 	while(1)
 	{
 		int med_number;
 		periph=peripheralinit();
 		med_number=medstime();
+		delay(1);
+		printf("medicine number = %d\n",med_number);	
 		if(med_number>0 && MED_FLAG==0)
 		{
 			MED_FLAG=1;
@@ -21,41 +23,43 @@ int main()
 		if(MED_FLAG)
 		{	
 			while(PIR_FLAG==0)
-            {
+			{
 
-                fprintf(periph.rled, %d,1);
-                fflush(periph.rled);
-                delay(1);
-                if(periph.pir>0 && PIR_FLAG==0)
-                {
-                    PIR_FLAG=1;
+				fprintf(periph.rled, "%d",1);
+				fflush(periph.rled);
+				delay(1);
+				if(periph.pir>0 && PIR_FLAG==0)
+				{
+					PIR_FLAG=1;
+					fprintf(periph.rled,"%d",0);
+					fflush(periph.rled);
 
-                }
-            }
+				}
+			}
 
-            fprintf(periph.gled, %d,1);
-            fflush(periph.gled);
-            delay(2);
-            //fprintf(p1.gled, %d,0);
-            //fflush(p1.gled);
-            //delay(2);
-            fprintf(periph.motorout1,%d,1);
-            fflush(periph.motorout1);
-            fprintf(periph.motorout1,%d,0);
-            fflush(periph.motorout2);
-            delay(2);
-            fprintf(periph.motorout1,%d,0);
-            fflush(periph.motorout1);
-            fprintf(periph.motorout1,%d,0);
-            fflush(periph.motorout2);
-            fprintf(periph.gled, %d,0);
-            fflush(periph.gled);
-            delay(2);
-        }
-        PIR_FLAG=0;
-        MED_FLAG=0;
-    }
-return 0;
+		    fprintf(periph.gled, "%d",1);
+		    fflush(periph.gled);
+		    delay(2);
+		    //fprintf(p1.gled, %d,0);
+		    //fflush(p1.gled);
+		    //delay(2);
+		    fprintf(periph.motorout1,"%d",1);
+		    fflush(periph.motorout1);
+		    fprintf(periph.motorout1,"%d",0);
+		    fflush(periph.motorout2);
+		    delay(2);
+		    fprintf(periph.motorout1,"%d",0);
+		    fflush(periph.motorout1);
+		    fprintf(periph.motorout1,"%d",0);
+		    fflush(periph.motorout2);
+		    fprintf(periph.gled, "%d",0);
+		    fflush(periph.gled);
+		    delay(2);
+        	}
+        	PIR_FLAG=0;
+        	MED_FLAG=0;
+    	}
+	return 0;
 }
 
 
