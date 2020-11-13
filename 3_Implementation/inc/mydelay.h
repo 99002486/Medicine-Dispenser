15808a622df1bbc8397c65a42afa9e0b90109e40
99002486 <71490592+99002486@users.noreply.github.com>
2020-11-13 10:07:21 +0530

    Create  mydelay.h

---
+++ 3_Implementation/inc/mydelay.h
@@ -0,0 +1,16 @@
+#ifndef __MYDELAY_H__
+#define __MYDELAY_H_
+
+#include <stdio.h>
+#include <time.h>
+
+/**
+@function-delay
+@param-int seconds
+@return-void
+@description - Executes a delay for number of seconds passed as the parameter
+**/
+
+void delay(int seconds);
+
+#endif
