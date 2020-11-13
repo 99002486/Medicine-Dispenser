4ed6397745e2126cc1811319bb329f5d97698ced
99002525 <71481208+99002525@users.noreply.github.com>
2020-11-13 10:40:31 +0530

    Create myspi.h

---
+++ 3_Implementation/inc/myspi.h
@@ -0,0 +1,10 @@
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
